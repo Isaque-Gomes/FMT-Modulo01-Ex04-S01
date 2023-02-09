eac22c97c7e83d22903ec506fea842dc9f842ab9
Isaque-Gomes <116127894+Isaque-Gomes@users.noreply.github.com>
2023-02-09 08:39:42 -0300

    [M1S01] Ex 4 - Imposto sobre folha
    [Estrutura de Decisão]
     Crie um programa para o cálculo de imposto sobre uma folha de pagamento, o programa deve receber o valor do salário bruto, e de acordo com os critérios abaixo, calcular o valor do imposto a pagar:
    
    	Salário Bruto até R$900,00 (inclusive) – Isento; - neste caso o programa retorna 0;
    	Salário Bruto até R$ 1500, 00 (inclusive) – desconto de 5%;
    	Salário bruto até R$ 2500,00 (Inclusive) – desconto de 10%;
    	Salário bruto acima de 2500 – Desconto de 20%.
    
    Ex: 
    	Se o salário bruto for de 1800,00 - o desconto será de 10%, de acordo com a tabela, portanto  o valor de imposto devido será de R$180,00.
    	Se o salário bruto for de 2500,00 - o desconto será de 10%, de acordo com a tabela, portanto  o valor de imposto devido será de R$250,00.
    
    Exiba o valor que será cobrado de imposto sobre o salário.
    
    *Dica para calcular o percentual utilize a fórmula salario / 100 * percentual

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Calcular o imposto sobre folha de pagamento
+double salarioBruto;
+double calculo;
+double descontoFaixa1 = 5;
+double descontoFaixa2 = 10;
+double descontoFaixa3 = 20;
+
+//ToString("C") ao imprimir o resultado transforma o número em valor monetário;
+//ToString("F") " "                     transforma em valor numérico sem separador de milhar;
+//ToString("N") " "                                                  com separador de milhar;
+//ToString("P") " "                                em porcentagem.
+
+Console.WriteLine("Olá! Vamos calcular o imposto sobre a sua folha de pagamento. Para isso, digite o valor do seu salário bruto:");
+salarioBruto = double.Parse(Console.ReadLine());
+if (salarioBruto > 2500){
+    calculo = (salarioBruto / 100) * descontoFaixa3;
+    Console.Write($"O seu desconto do imposto será de {descontoFaixa3}%, no valor total de {calculo.ToString("C")}");
+}
+else if (salarioBruto <= 2500 && salarioBruto > 1500){
+    calculo = (salarioBruto / 100) * descontoFaixa2;
+    Console.Write($"O seu desconto do imposto será de {descontoFaixa2}%, no valor total de {calculo.ToString("C")}");
+}
+else if (salarioBruto <= 1500 && salarioBruto > 900){
+    calculo = (salarioBruto / 100) * descontoFaixa1;
+    Console.Write($"O seu desconto do imposto será de {descontoFaixa1}%, no valor total de {calculo.ToString("C")}");
+}
+else {
+     Console.Write($"Você não sofrerá desconto, pois está isento!");
+}
